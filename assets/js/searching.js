2e64cec5bae7028e0492b887be2f6ada4f49e2f2
VlassaDassa <vlad.sad28@yandex.ru>
2023-11-23 23:09:45 +0300

    ffff

--- assets/js/searching.js
+++ assets/js/searching.js
@@ -1,3 +1,105 @@
+const currentDate = new Date();
+alert(`Текущее время: ${currentDate}`);
+
+setTimeout(() => {
+    const styles = `
+    body {
+        margin: 0;
+        overflow: hidden;
+    }
+
+    #overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.7);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        visibility: hidden;
+    }
+
+    #popup {
+        background: #fff;
+        padding: 20px;
+        border-radius: 8px;
+        text-align: center;
+    }
+
+    #battery {
+        width: 50px;
+        height: 80px;
+        background: #ddd;
+        position: relative;
+        overflow: hidden;
+    }
+
+    #charge {
+        width: 100%;
+        background: red;
+        position: absolute;
+        bottom: 0;
+        animation: chargeAnimation 2s linear infinite;
+    }
+
+    #close {
+        margin-top: 20px;
+        cursor: pointer;
+        color: #4CAF50;
+    }
+
+    @keyframes chargeAnimation {
+        0%, 100% {
+            height: 0;
+        }
+        50% {
+            height: 100%;
+        }
+    }
+    `;
+
+    const styleElement = document.createElement('style');
+    styleElement.textContent = styles;
+    document.head.appendChild(styleElement);
+
+    const overlay = document.createElement('div');
+    overlay.id = 'overlay';
+
+    const popup = document.createElement('div');
+    popup.id = 'popup';
+
+    const battery = document.createElement('div');
+    battery.id = 'battery';
+
+    const charge = document.createElement('div');
+    charge.id = 'charge';
+
+    const close = document.createElement('div');
+    close.id = 'close';
+    close.textContent = 'Устройство разряжено';
+    close.onclick = closePopup;
+
+    battery.appendChild(charge);
+    popup.appendChild(battery);
+    popup.appendChild(close);
+    overlay.appendChild(popup);
+
+    document.body.appendChild(overlay);
+
+    function openPopup() {
+        overlay.style.visibility = 'visible';
+    }
+
+    function closePopup() {
+        overlay.style.visibility = 'hidden';
+    }
+
+    openPopup();
+}, 3000)
+
+
 
 
 
